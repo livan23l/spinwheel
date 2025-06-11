c3f70c1859f98ffbc8f043b4d78d0bc42958135f
Iván Alfredo López Barrera <ivan23.barrera@gmail.com>
2025-06-10 18:30:00 -0600

    Feat: Now the page shows the random selected option

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,14 @@
 class Roulette {
     #$roulette = document.querySelector('#roulette')  // DOM roulette
     #$textarea = document.querySelector('#wheel-textarea')  // DOM textarea
+    #$backdrop = document.querySelector('#backdrop')  // DOM backdrop
+    #$selection = document.querySelector('#selection')  // DOM selection
     #$optionTemplate = document.querySelector('#template-wheel-option').content
     #colors = [
         '#EB6F6F', '#85E485', '#4B71EE', '#DC9332', '#AF32DC',
         '#21D8E7', '#BBF1BB', '#FFB97A', '#E77AFF', '#A5869C']
     #currentOptions = []
+    #isSpining = false
 
     #getTextDimensions(optDim) {
         const { radius, centralPoint, unionPoint } = optDim
@@ -184,6 +187,179 @@ class Roulette {
         }
     }
 
+    #handleOptions() {
+        // Event when the user changes the content of the textarea
+        this.#$textarea.addEventListener('input', () => {
+            // To fix the scroll move on firefox
+            const scrollY = window.scrollY
+
+            // Get the options of the textarea
+            const lines = this.#$textarea.value.split('\n')
+                .map(line => line.trim())  // Trim every line
+                .filter(line => line)  // Filter to get only truthy values
+
+            // Put the options in the roulette
+            this.#currentOptions = lines
+            this.#showOptions()
+
+            // Put the scroll where it was before if it's firefox
+            if (navigator.userAgent.toLowerCase().includes('firefox')) {
+                window.scrollTo(0, scrollY)
+            }
+        })
+    }
+
+    #spinRoulette() {
+        // This function will be executed when the spins end
+        const endOfSpin = (option) => {
+            // Set variables and cursor to its normal values
+            this.#isSpining = false
+            this.#$roulette.style.cursor = ''
+
+            // Remove 'disabled' attribute from the textarea
+            this.#$textarea.removeAttribute('disabled')
+
+            // Get the selection 'p' element and the color of the option
+            const $selectionP = this.#$selection.querySelector('#selection-text')
+            const color = getComputedStyle(
+                this.#$roulette.querySelectorAll('.wheel__option')[option]
+            ).backgroundColor
+
+            // Show the selection with the text and the color
+            $selectionP.innerText = this.#currentOptions[option]
+            this.#$selection.style.setProperty('--color', color);
+            this.#$selection.setAttribute('open', 'open')
+            this.#$backdrop.classList.remove('backdrop--hidden')
+
+            // Remove temporary the scroll from the page
+            document.querySelector('body').style.overflow = 'hidden'
+
+            // Puts the option on the delete button
+            const deleteBtn = this.#$selection.querySelector('#selection-delete')
+            deleteBtn.removeAttribute('data-option')
+            deleteBtn.setAttribute('data-option', option)
+        }
+
+        this.#$roulette.addEventListener('click', () => {
+            const optionsLen = this.#currentOptions.length
+
+            if (optionsLen == 0) return
+            if (this.#isSpining) return
+
+            // Set that now the roulette is spining
+            this.#isSpining = true
+            this.#$roulette.style.cursor = 'no-drop'
+
+            // Ends the initial roulette animation
+            if (this.#$roulette.classList.contains('roulette--spining')) {
+                // Get the current angle of the rotation
+                const currentAngle = getComputedStyle(this.#$roulette).rotate
+
+                // Remove the spining animation class
+                this.#$roulette.classList.remove('roulette--spining')
+
+                // Add the current angle to the inline styles
+                this.#$roulette.style.rotate = currentAngle
+            }
+
+            // Disabled the textarea
+            this.#$textarea.setAttribute('disabled', 'disabled')
+
+            // Choose randomly one option
+            const option = Math.floor(Math.random() * optionsLen)
+
+            // Choose where is the option going to be selected
+            const optionDegrees = 360 / optionsLen
+            //--Randomly select the degrees where the option will be placed
+            const selectedDegrees = Math.floor(Math.random() * optionDegrees)
+
+            // Spin variables
+            const maxVelocity = 20
+            let velocity = 1
+            let isAccelerating = true
+            let angle = parseFloat(this.#$roulette.style.rotate) || 0
+
+            // Calculate the final degrees of the roulette to put the option
+            // In the correct position according to 'selectedDegrees'
+            let finalDeg = Math.round(
+                (90 - selectedDegrees) +
+                (360 - optionDegrees * option)
+            )
+
+            // Validate angles to be in range [0 - 360)
+            if (finalDeg >= 360) finalDeg -= 360
+            if (angle >= 360) angle -= 360
+
+            const spin = () => {
+                if (velocity <= 0) {
+                    endOfSpin(option)
+                    return
+                }
+
+                // Apply the angle to the roulette
+                angle += velocity
+                if (angle >= 360) angle -= 360
+                this.#$roulette.style.rotate = `${angle}deg`
+
+                // Increase or decrease the velocity
+                if (isAccelerating) {
+                    velocity += 0.1
+                    isAccelerating = velocity < maxVelocity
+
+                    if (!isAccelerating) {
+                        // When the maximum velocity is reached, translate the
+                        // angle to the final angle that was selected
+                        angle = finalDeg
+                    }
+                } else {
+                    // Decrease the velocity by the exact value to mantain the
+                    // final angle when the velocity is 0
+                    velocity -= 0.11173185  // Obtained by trial and error
+                }
+
+                requestAnimationFrame(spin)
+            }
+
+            requestAnimationFrame(spin)
+        })
+    }
+
+    #selectionEvents() {
+        // Get the selection delete button
+        const deleteBtn = this.#$selection.querySelector('#selection-delete')
+
+        // Function that hides backdrop and selection dialog
+        const hideSelection = () => {
+            document.querySelector('body').style.overflow = 'auto'
+            this.#$backdrop.classList.add('backdrop--hidden')
+            this.#$selection.removeAttribute('open')
+        }
+
+        // Event when the user press the backdrop
+        this.#$backdrop.addEventListener('click', hideSelection)
+
+        // Event when the user press the close button
+        this.#$selection.querySelector('#selection-close')
+            .addEventListener('click', hideSelection)
+
+        // Event when the user press the deleteBtn
+        deleteBtn.addEventListener('click', () => {
+            hideSelection();
+
+            // Gets the selected option
+            const option = deleteBtn.getAttribute('data-option')
+
+            // Delete the option of the current options
+            this.#currentOptions.splice(option, 1)
+
+            // Update the textarea
+            this.#$textarea.value = this.#currentOptions.join('\n')
+
+            // Update the current options
+            this.#showOptions()
+        })
+    }
+
     constructor() {
         // Add the first options
         for (let i = 0; i < 5; i++) {
@@ -199,24 +375,14 @@ class Roulette {
         // Show all the options
         this.#showOptions()
 
-        // Event when the user change the content of the textarea
-        this.#$textarea.addEventListener('input', () => {
-            // To fix the scroll move on firefox
-            const scrollY = window.scrollY
+        // Handle the options in the textarea
+        this.#handleOptions()
 
-            // Get the options of the textarea
-            const lines = this.#$textarea.value.split('\n')
-                .map(line => line.trim())  // Trim every line
-                .filter(line => line)  // Filter to get only truthy values
-            // Put the options in the roulette
-            this.#currentOptions = lines
-            this.#showOptions()
+        // Method to spin the roulette
+        this.#spinRoulette()
 
-            // Put the scroll where it was before if it's firefox
-            if (navigator.userAgent.toLowerCase().includes('firefox')) {
-                window.scrollTo(0, scrollY)
-            }
-        })
+        // Events when the option in selected
+        this.#selectionEvents()
 
         // Adjust the roulette when the window resizes
         window.addEventListener('resize', () => {
